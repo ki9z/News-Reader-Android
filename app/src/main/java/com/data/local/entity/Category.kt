e0c612096b4cc38b4ffa38c03767c4f995a0a8c4
Bcnn-Cof <227569044+Bcnn-Cof@users.noreply.github.com>
2026-04-12 22:13:28 +0700

    Create Category.kt

---
+++ app/src/main/java/com/data/local/entity/Category.kt
@@ -0,0 +1,9 @@
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.ForeignKey
+@Entity(tableName="categories")
+data class Category(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String
+)
