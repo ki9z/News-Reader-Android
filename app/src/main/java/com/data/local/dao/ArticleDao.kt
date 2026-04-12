368a8400cf341b80834b40227ff994c6a4f127d9
Bcnn-Cof <227569044+Bcnn-Cof@users.noreply.github.com>
2026-04-12 22:17:00 +0700

    Update ArticleDao.kt

--- app/src/main/java/com/data/local/dao/ArticleDao.kt
+++ app/src/main/java/com/data/local/dao/ArticleDao.kt
@@ -1,4 +1,28 @@
-package com.data.local.dao
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import androidx.room.Delete
+import androidx.room.OnConflictStrategy
+import com.example.myapplication.data.local.entity.*
 
-class ArticleDao {
+@Dao
+interface ArticleDao{
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(article: Article) : Long
+
+    @Query("SELECT * FROM articles WHERE remoteId = :remoteId LIMIT 1")
+    suspend fun findByRemoteId(remoteId: String): Article?
+
+    @Query("SELECT * FROM articles WHERE categoryId = :categoryId")
+    suspend fun getByCategory(categoryId: Long): List<Article>
+
+    @Query("SELECT * FROM articles")
+    suspend fun getAll(): List<Article>
+
+    @Delete
+    suspend fun delete(article: Article)
+
+    @Update
+    suspend fun update(article: Article)
 }
