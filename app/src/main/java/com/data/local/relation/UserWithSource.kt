1a7df062c84504315d8e54a6f3965d54fb52dacf
Bcnn-Cof <227569044+Bcnn-Cof@users.noreply.github.com>
2026-04-19 08:31:26 +0700

    Create UserWithSource.kt

---
+++ app/src/main/java/com/data/local/relation/UserWithSource.kt
@@ -0,0 +1,17 @@
+package com.data.local.relation
+import com.data.local.entity.*
+import androidx.room.*
+data class UserWithSource(
+    @Embedded val user: User,
+
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "id",
+        associateBy = Junction(
+            value = SourceUser::class,
+            parentColumn = "userId",
+            entityColumn = "sourceId"
+        )
+    )
+    val sources: List<Source>
+)
